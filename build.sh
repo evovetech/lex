541cd59299c0948fb4e357774e6fd29ad922652c
Layne Penney <layne@evove.tech>
2018-08-23 01:17:37 -0500

    update build script to link with llvm

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
-cd cmd
+function gopath() {
+    local paths=( $( tr ':' '\n' <<< "$GOPATH" ) )
+    printf "${paths[0]}"
+}
 
-go build -o ../bin/lex
+function build() {
+    local args=(
+        build "$@"
+        -ldflags="-r $(gopath)/src/llvm.org/llvm/bindings/go/llvm/workdir/llvm_build/lib"
+        -o ../bin/lex
+        .
+    )
+    echo '$' "go ${args[@]}"
+    cd cmd && go "${args[@]}"
+}
+
+# main
+build "$@"
