5f37baec53e80c6a58fbb43e92da199facf31a85
Layne Penney <layne@evove.tech>
2019-03-18 14:37:43 -0500

    min/max

--- sample/main.cpp
+++ sample/main.cpp
@@ -3,9 +3,13 @@
 extern "C" {
     double sum(double, double);
     double average(double, double);
+    double min(double, double);
+    double max(double, double);
 }
 
 int main() {
     std::cout << "sum of 3.0 and 4.0: " << sum(3, 4) << std::endl;
-    std::cout << "average of 3.0 and 4.0: " << average(3.0, 4.0) << std::endl;
+    std::cout << "average of 3.0 and 4.0: " << average(3.0, 4) << std::endl;
+    std::cout << "min of 3.0 and 4.0: " << min(3, 4.0) << std::endl;
+    std::cout << "max of 3.0 and 4.0: " << max(3.0, 4.0) << std::endl;
 }
--- sample/output.ll
+++ sample/output.ll
@@ -15,3 +15,17 @@ entry:
   %multmp = fmul double %calltmp, 5.000000e-01
   ret double %multmp
 }
+
+define double @min(double %x, double %y) {
+entry:
+  %cmptmp = fcmp ult double %x, %y
+  %x.y = select i1 %cmptmp, double %x, double %y
+  ret double %x.y
+}
+
+define double @max(double %x, double %y) {
+entry:
+  %cmptmp = fcmp ult double %x, %y
+  %y.x = select i1 %cmptmp, double %y, double %x
+  ret double %y.x
+}
